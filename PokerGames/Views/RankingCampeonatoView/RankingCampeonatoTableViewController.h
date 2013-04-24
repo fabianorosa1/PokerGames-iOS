0e90862869e9280cab06b279e68da7d5ebca3eb0
Fabiano <fabianorosa1@gmail.com>
2013-04-23 23:23:54 -0300

    Adicionado configuracao login

---
+++ PokerGames/Views/RankingCampeonatoView/RankingCampeonatoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RankingCampeonatoTableViewController.h
+//  PokerGames
+//
+//  Created by Fabiano Rosa on 23/04/13.
+//  Copyright (c) 2013 Fabiano Rosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RankingCampeonatoTableViewController : UITableViewController
+
+@end
